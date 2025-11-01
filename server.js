982c6a91f1a95456e63abbd5e5290a013ea53ec7
DeivanaiAnandan <deivanai.anandan@gmail.com>
2025-11-01 16:42:48 +0530

    Prepare for Render deployment

--- server.js
+++ server.js
@@ -25,14 +25,6 @@ if (process.env.NODE_ENV === 'development') {
 // API routes
 app.use('/api/transactions', transactionRoutes);
 
-// ---------- ✅ Production setup ----------
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-//   app.get('/.*/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './client/build')));
@@ -53,41 +45,4 @@ app.listen(PORT, () =>
   )
 );
 
-// import path from 'path';
-// import express from "express";
-// import colors from "colors";
-// import morgan from "morgan";
-// import dotenv from "dotenv";
-// import transactionRoutes from "./routes/transactionRoutes.js";
-// import { connectDB } from "./config/db.js";
 
-
-// dotenv.config();
-// //if we use a seperate folder structure like config/.env then we need to add path
-// //dotenv.config({ path: './config/.env' });
-
-// connectDB();
-
-// const app = express();
-// app.use(express.json());
-// if(process.env.NODE_ENV === 'development'){
-// app.use(morgan('dev'));
-// }
-
-// // app.get("/", (req, res) => res.send("Hello"));
-// app.use("/api/transactions", transactionRoutes);
-// //only below routes this line
-// if(process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-
-//   app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-// }
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () =>
-//   console.log(
-//     `Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-//       .bold
-//   )
-// );
